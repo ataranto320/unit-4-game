2cf9c66c92d41afeefbf0e7c94c8969beea8bdfb
Austin T <austin.taranto@gmail.com>
2019-05-13 13:47:36 -0400

    added reset function

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -9,3 +9,22 @@ var gem2 = Math.floor(Math.random()*11+1)
 var gem3 = Math.floor(Math.random()*11+1)
 var gem4 = Math.floor(Math.random()*11+1)
 
+var total = 0;
+var wins = 0;
+var losses = 0;
+
+$('#numberWins').text(wins);
+$('#numberLosses').text(losses);
+
+//reset game
+function reset() {
+    Random = Math.floor(Math.random()*101+19);
+    console.log(Random)
+    $('#randomNumber').text(Random);
+    gem1 = Math.floor(Math.random()*11+1);
+    gem2 = Math.floor(Math.random()*11+1);
+    gem3 = Math.floor(Math.random()*11+1);
+    gem4 = Math.floor(Math.random()*11+1);
+    userTotal = 0;
+    $('#finalTotal').text(userTotal);
+}
