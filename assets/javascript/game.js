79749681fd88a63a03095fc9545b701426e6fec8
Austin T <austin.taranto@gmail.com>
2019-05-13 12:35:46 -0400

    math random for java

---
+++ assets/javascript/game.js
@@ -0,0 +1,11 @@
+// select random number at start of game
+var Random = Math.floor(Math.random()*101+19)
+
+$('#randomNumber').text(Random);
+
+//random numbers for jewels
+var gem1 = Math.floor(Math.random()*11+1)
+var gem2 = Math.floor(Math.random()*11+1)
+var gem3 = Math.floor(Math.random()*11+1)
+var gem4 = Math.floor(Math.random()*11+1)
+
